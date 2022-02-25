2161029a592743dfffb34132c58a7e7b1293d409
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2022-02-25 15:00:13 +0530

    try

---
+++ models/yolo_layer_2.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from utils.utils import bboxes_iou
+
+
+class YOLOLayer(nn.Module):
+    """
+    detection layer corresponding to yolo_layer.c of darknet
+    """
+    def __init__(self, cfg,layer_no, in_ch,):
+        """
+        Args:
+            cfg (dict) : Configuration file.
+                ANCHORS (list of tuples) :
+                ANCH_MASK:  (list of int list): index indicating the anchors to be
+                    used in YOLO layers. One of the mask group is picked from the list.
+                N_CLASSES (int): number of classes
+            layer_no (int): YOLO layer number - one from (0, 1, 2).
+            in_ch (int): number of input channels.
+            ignore_thre (float): threshold of IoU above which objectness training is ignored.
+        """
+
+        super(YOLOLayer, self).__init__()
+        self.strides = [32, 16, 8] # fixed
+        self.stride = self.strides[layer_no]
+
+        self.cfg = cfg 
+        self.n_classes = self.cfg['MODEL']['N_CLASSES']
+        self.n_ch = 5 + self.n_classes
+        self.ignore_thre = self.cfg['TRAIN']['IGNORETHRE']
+        self.batchsize = self.cfg['DATA']['BATCHSIZE']
+        
+        self.dtype=torch.FloatTensor
+        self.anchors = self.cfg['MODEL']['ANCHORS']
+        self.anch_mask = self.cfg['MODEL']['ANCH_MASK'][layer_no]
+        self.n_anchors = len(self.anch_mask)
+        self.all_anchors_grid = [(w / self.stride, h / self.stride)
+                                 for w, h in self.anchors]
+        self.masked_anchors = [self.all_anchors_grid[i]
+                               for i in self.anch_mask]
+        
+        self.conv = nn.Conv2d(in_channels=in_ch,
+                              out_channels=self.n_anchors * (self.n_ch),
+                              kernel_size=1, stride=1, padding=0)
+        
+        self.Lambda = 0.5
+    
+    def bce_loss(self,weight=None):
+        return nn.BCELoss(weight=weight,size_average=False)
+    
+    @property
+    def l2_loss(self):
+        return nn.MSELoss(size_average=False)
+    
+    def get_pred(self,fsize,output):
+        x_shift = self.dtype(np.broadcast_to(np.arange(fsize, dtype=np.float32), output.shape[:4]))
+        y_shift = self.dtype(np.broadcast_to(np.arange(fsize, dtype=np.float32).reshape(fsize, 1), output.shape[:4]))
+        masked_anchors = np.array(self.masked_anchors)
+        w_anchors = self.dtype(np.broadcast_to(np.reshape(masked_anchors[:, 0], (1, self.n_anchors, 1, 1)), output.shape[:4]))
+        h_anchors = self.dtype(np.broadcast_to(np.reshape(masked_anchors[:, 1], (1, self.n_anchors, 1, 1)), output.shape[:4]))
+
+        pred = output.clone()
+        pred[..., 0] += x_shift
+        pred[..., 1] += y_shift
+        pred[..., 2] = torch.exp(pred[..., 2]) * w_anchors
+        pred[..., 3] = torch.exp(pred[..., 3]) * h_anchors
+        return pred
+    
+    def get_output(self,xin):
+        output = self.conv(xin)
+        fsize = output.shape[2]
+        output = output.view(self.batchsize, self.n_anchors, self.n_ch, fsize, fsize)
+        output = output.permute(0, 1, 3, 4, 2)  # .contiguous()
+        # logistic activation for xy, obj, cls
+        output[..., np.r_[:2, 4:self.n_ch]] = torch.sigmoid(output[..., np.r_[:2, 4:self.n_ch]])
+        return output,fsize
+    
+    def get_best_mask(self,truth_box):
+        ref_anchors = np.zeros((len(self.all_anchors_grid), 4))
+        ref_anchors[:, 2:] = np.array(self.all_anchors_grid)
+        ref_anchors = torch.FloatTensor(ref_anchors)
+        anchor_ious_all = bboxes_iou(truth_box.cpu(),ref_anchors)
+        best_n_all = np.argmax(anchor_ious_all, axis=1)
+        best_n_mask = ((best_n_all == self.anch_mask[0]) | (best_n_all == self.anch_mask[1]) | (best_n_all == self.anch_mask[2]))
+        return best_n_all,best_n_mask
+    
+    def get_best_iou(self,pred,truth_box):
+        pred_ious = bboxes_iou(pred.contiguous().view(-1, 4), truth_box, xyxy=False)
+        pred_best_iou, _ = pred_ious.max(dim=1)
+        pred_best_iou = (pred_best_iou > self.ignore_thre)
+        pred_best_iou = pred_best_iou.view(pred.shape[:3])
+        return pred_best_iou
+    
+    def process(self,x,obj_mask,tgt_mask,tgt_scale):
+        x[..., 4] *= obj_mask
+        x[..., np.r_[0:4, 5:self.n_ch]] *= tgt_mask
+        x[..., 2:4] *= tgt_scale
+        return x
+    
+    def get_losses(self,output,target,obj_mask,tgt_mask,tgt_scale):
+        output= self.process(output,obj_mask,tgt_mask,tgt_scale)
+        target= self.process(target,obj_mask,tgt_mask,tgt_scale)
+        wbce = self.bce_loss(weight=tgt_scale*tgt_scale)  # weighted BCEloss
+        bce = self.bce_loss()
+        loss_xy = wbce(output[..., :2], target[..., :2])
+        loss_wh = self.l2_loss(output[..., 2:4], target[..., 2:4])*self.Lambda
+        loss_obj = bce(output[..., 4], target[..., 4])
+        loss_cls = bce(output[..., 5:], target[..., 5:])
+        loss_l2 = self.l2_loss(output, target)
+        loss = loss_xy + loss_wh + loss_obj + loss_cls
+        # return loss, loss_xy, loss_wh, loss_obj, loss_cls, loss_l2
+        return loss 
+
+    def forward(self, xin,labels=None,end=False):
+        """
+        In this
+        Args:
+            xin (torch.Tensor): input feature map whose size is :math:`(N, C, H, W)`, \
+                where N, C, H, W denote batchsize, channel width, height, width respectively.
+            labels (torch.Tensor): label data whose size is :math:`(N, K, 5)`. \
+                N and K denote batchsize and number of labels.
+                Each label consists of [class, xc, yc, w, h]:
+                    class (float): class index.
+                    xc, yc (float) : center of bbox whose values range from 0 to 1.
+                    w, h (float) : size of bbox whose values range from 0 to 1.
+        Returns:
+            loss (torch.Tensor): total loss - the target of backprop.
+            loss_xy (torch.Tensor): x, y loss - calculated by binary cross entropy (BCE) \
+                with boxsize-dependent weights.
+            loss_wh (torch.Tensor): w, h loss - calculated by l2 without size averaging and \
+                with boxsize-dependent weights.
+            loss_obj (torch.Tensor): objectness loss - calculated by BCE.
+            loss_cls (torch.Tensor): classification loss - calculated by BCE for each class.
+            loss_l2 (torch.Tensor): total l2 loss - only for logging.
+        """
+        self.dtype = torch.cuda.FloatTensor if xin.is_cuda else torch.FloatTensor
+        output,fsize = self.get_output(xin)
+    
+        # calculate pred - xywh obj cls
+        pred = self.get_pred(fsize,output)
+
+        if labels is None:  # not training
+            pred[..., :4] *= self.stride
+            return pred.contiguous().view(self.batchsize, -1, self.n_ch).data
+        if not end:
+            return pred.contiguous().view(self.batchsize, -1, self.n_ch).data
+        else :
+            return self.forward_losses(fsize,labels,output,pred)
+
+    def forward_losses(self,fsize,labels,output,pred):
+        # target assignment
+        tgt_mask = torch.zeros(self.batchsize, self.n_anchors,fsize, fsize,4 + self.n_classes).type(self.dtype)
+        obj_mask = torch.ones(self.batchsize, self.n_anchors,fsize, fsize).type(self.dtype)
+        tgt_scale = torch.zeros(self.batchsize, self.n_anchors,fsize, fsize, 2).type(self.dtype)
+        target = torch.zeros(self.batchsize, self.n_anchors,fsize, fsize, self.n_ch).type(self.dtype)
+
+        labels = labels.cpu().data
+        nlabel = (labels.sum(dim=2) > 0).sum(dim=1)  # number of objects
+
+        truth_x_all = labels[:, :, 1] * fsize
+        truth_y_all = labels[:, :, 2] * fsize
+        truth_w_all = labels[:, :, 3] * fsize
+        truth_h_all = labels[:, :, 4] * fsize
+        truth_i_all = truth_x_all.to(torch.int16).numpy()
+        truth_j_all = truth_y_all.to(torch.int16).numpy()
+
+        for b in range(self.batchsize):
+            n = int(nlabel[b])
+            if n == 0:
+                continue
+            truth_box = self.dtype(np.zeros((n, 4)))
+            truth_box[:n, 2] = truth_w_all[b, :n]
+            truth_box[:n, 3] = truth_h_all[b, :n]
+            truth_i = truth_i_all[b, :n]
+            truth_j = truth_j_all[b, :n]
+            truth_box[:n, 0] = truth_x_all[b, :n]
+            truth_box[:n, 1] = truth_y_all[b, :n]
+            
+            # calculate iou between truth and reference anchors
+            best_n_all,best_n_mask = self.get_best_mask(truth_box)
+            best_n = best_n_all % 3
+
+            pred_best_iou = self.get_best_iou(pred[b],truth_box)
+            # set mask to zero (ignore) if pred matches truth
+            obj_mask[b] = 1 - pred_best_iou.type(self.dtype)
+
+            if sum(best_n_mask) == 0:
+                continue
+
+            for ti in range(best_n.shape[0]):
+                if best_n_mask[ti] == 1:
+                    i, j = truth_i[ti], truth_j[ti]
+                    a = best_n[ti]
+                    obj_mask[b, a, j, i] = 1
+                    tgt_mask[b, a, j, i, :] = 1
+                    target[b, a, j, i, 0] = truth_x_all[b, ti] - truth_x_all[b, ti].to(torch.int16).to(torch.float)
+                    target[b, a, j, i, 1] = truth_y_all[b, ti] - truth_y_all[b, ti].to(torch.int16).to(torch.float)
+                    target[b, a, j, i, 2] = torch.log(truth_w_all[b, ti] / torch.Tensor(self.masked_anchors)[best_n[ti], 0] + 1e-16)
+                    target[b, a, j, i, 3] = torch.log(truth_h_all[b, ti] / torch.Tensor(self.masked_anchors)[best_n[ti], 1] + 1e-16)
+                    target[b, a, j, i, 4] = 1
+                    target[b, a, j, i, 5 + labels[b, ti,0].to(torch.int16).numpy()] = 1
+                    tgt_scale[b, a, j, i, :] = torch.sqrt(2 - truth_w_all[b, ti] * truth_h_all[b, ti] / fsize / fsize)
+
+        # loss calculation
+        return self.get_losses(output,target,obj_mask,tgt_mask,tgt_scale)
---
+++ models/yolov3_2.py
@@ -0,0 +1,163 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from collections import defaultdict
+
+from models.yolo_layer_2 import YOLOLayer
+from models.resblock import *
+from utils.parse_yolo_weights import parse_yolo_weights
+
+class YOLOv3(nn.Module):
+    """
+    YOLOv3 model module. The module list is defined by create_yolov3_modules function. \
+    The network returns loss values from three YOLO layers during training \
+    and detection results during test.
+    """
+    def __init__(self,cfg,):
+        """
+        Initialization of YOLOv3 class.
+        Args:
+            cfg (dict): Configuration file used in YOLOLayer.
+        """
+        super(YOLOv3, self).__init__()
+        self.cfg = cfg
+        if self.cfg['MODEL']['TYPE'].upper() == 'YOLOV3'.upper():
+            self.module_list = self.create_yolov3_modules(cfg)
+        else:
+            raise Exception('Model name {} is not available'.format(cfg['MODEL']['TYPE']))
+        self.parse_weights()
+        # self.loss_dict = defaultdict(float)
+
+    def create_yolov3_modules(self,cfg):
+        """
+        Build yolov3 layer modules.
+        Args:
+            cfg (dict): Configuration file.
+            See YOLOLayer class for details.
+        Returns:
+            mlist (ModuleList): YOLOv3 module list.
+        """
+
+        # DarkNet53
+        mlist = nn.ModuleList()
+        mlist.append(add_conv(in_ch=3, out_ch=32, ksize=3, stride=1))
+        mlist.append(add_conv(in_ch=32, out_ch=64, ksize=3, stride=2))
+        mlist.append(resblock(ch=64))
+        mlist.append(add_conv(in_ch=64, out_ch=128, ksize=3, stride=2))
+        mlist.append(resblock(ch=128, nblocks=2))
+        mlist.append(add_conv(in_ch=128, out_ch=256, ksize=3, stride=2))
+        mlist.append(resblock(ch=256, nblocks=8))    # shortcut 1 from here
+        mlist.append(add_conv(in_ch=256, out_ch=512, ksize=3, stride=2))
+        mlist.append(resblock(ch=512, nblocks=8))    # shortcut 2 from here
+        mlist.append(add_conv(in_ch=512, out_ch=1024, ksize=3, stride=2))
+        mlist.append(resblock(ch=1024, nblocks=4))
+
+        # YOLOv3
+        mlist.append(resblock(ch=1024, nblocks=2, shortcut=False))
+        mlist.append(add_conv(in_ch=1024, out_ch=512, ksize=1, stride=1))
+        # 1st yolo branch
+        mlist.append(add_conv(in_ch=512, out_ch=1024, ksize=3, stride=1))
+        mlist.append(YOLOLayer(cfg, layer_no=0, in_ch=1024))
+
+        mlist.append(add_conv(in_ch=512, out_ch=256, ksize=1, stride=1))
+        mlist.append(nn.Upsample(scale_factor=2, mode='nearest'))
+        mlist.append(add_conv(in_ch=768, out_ch=256, ksize=1, stride=1))
+        mlist.append(add_conv(in_ch=256, out_ch=512, ksize=3, stride=1))
+        mlist.append(resblock(ch=512, nblocks=1, shortcut=False))
+        mlist.append(add_conv(in_ch=512, out_ch=256, ksize=1, stride=1))
+        # 2nd yolo branch
+        mlist.append(add_conv(in_ch=256, out_ch=512, ksize=3, stride=1))
+        mlist.append(YOLOLayer(cfg, layer_no=1, in_ch=512))
+
+        mlist.append(add_conv(in_ch=256, out_ch=128, ksize=1, stride=1))
+        mlist.append(nn.Upsample(scale_factor=2, mode='nearest'))
+        mlist.append(add_conv(in_ch=384, out_ch=128, ksize=1, stride=1))
+        mlist.append(add_conv(in_ch=128, out_ch=256, ksize=3, stride=1))
+        mlist.append(resblock(ch=256, nblocks=2, shortcut=False))
+        mlist.append(YOLOLayer(cfg,layer_no=2, in_ch=256))
+        return mlist
+
+    def forward(self,x,targets=None,cuda=True):
+        """
+        Forward path of YOLOv3.
+        Args:
+            x (torch.Tensor) : input data whose shape is :math:`(N, C, H, W)`, \
+                where N, C are batchsize and num. of channels.
+            targets (torch.Tensor) : label array whose shape is :math:`(N, 50, 5)`
+
+        Returns:
+            training:
+                output (torch.Tensor): loss tensor for backpropagation.
+            test:
+                output (torch.Tensor): concatenated detection results.
+        """
+        train = targets is not None
+        if train:
+            dtype = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+            x = Variable(x.type(dtype))
+            targets = Variable(targets.type(dtype), requires_grad=False)
+        else:
+            output = []
+        route_layers = []
+        for i, module in enumerate(self.module_list):
+            # Intermediate yolo layers
+            if i in [14,22]:
+                if train:
+                    x = module(x, targets)
+                else:
+                    x = module(x)
+                    output.append(x)
+            else:
+                x = module(x)
+
+            # route layers
+            if i in [6, 8, 12, 20]:
+                route_layers.append(x)
+            if i == 14:
+                x = route_layers[2]
+            if i == 22:  # yolo 2nd
+                x = route_layers[3]
+            if i == 16:
+                x = torch.cat((x, route_layers[1]), 1)
+            if i == 24:
+                x = torch.cat((x, route_layers[0]), 1)
+            
+            # Final yolo layer
+            if i == 28:
+                if train:
+                    loss = module(x,targets,end=True) 
+                else: 
+                    x = module(x)
+                    output.append(x)
+        if train:
+            return loss 
+        else:
+            return torch.cat(output, 1)
+    
+    def parse_weights(self):
+        weights_path = self.cfg['MODEL']['WEIGHTS_PATH']
+        if weights_path is not None:
+            print("loading darknet weights from ....", weights_path)
+            parse_yolo_weights(self,weights_path)
+    
+    def load_state(self,state):
+        if 'model_state_dict' in state.keys():
+            self.load_state_dict(state['model_state_dict'])
+        else:
+            self.load_state_dict(state)
+    
+    def get_params(self):
+        # set weight decay only on conv.weight
+        batch_size = self.cfg['DATA']['BATCHSIZE']
+        subdivision = self.cfg['DATA']['SUBDIVISION']
+        decay = self.cfg['SOLVER']['DECAY']
+        params_dict = dict(self.named_parameters())
+        params = []
+        for key, value in params_dict.items():
+            if 'conv.weight' in key:
+                params += [{'params':value, 'weight_decay':decay * batch_size * subdivision}]
+            else:
+                params += [{'params':value, 'weight_decay':0.0}]
+        return params 
+
+
