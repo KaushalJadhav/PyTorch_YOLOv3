59842fa276b997e890aac1285bdb5b7971b1dcb4
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2022-04-22 16:28:59 +0530

    debug

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -2,27 +2,27 @@ import torch.optim as optim
 
 class LambdaLRScheduler():
     def __init__(self,optimizer,cfg,iter_state=0):
-        self.scheduler = optim.lr_scheduler.LambdaLR(optimizer,self.burnin_schedule)
         self.iter_state = iter_state
         self.cfg=cfg
         self.iter_size = self.cfg['SOLVER']['MAXITER']
         self.batch_size = self.cfg['DATA']['BATCHSIZE']
         self.subdivision = self.cfg['DATA']['SUBDIVISION']
         self.effective_batch_size = self.batch_size*self.subdivision
-        self.burn_in = self.cfg['SOLVER']['BURN_IN']
-        self.steps = eval(self.cfg['SOLVER']['STEPS'])
-    
-    # Learning rate setup
-    def burnin_schedule(self,i):
-        if i < self.burn_in:
-            factor = pow(i / self.burn_in, 4)
-        elif i < self.steps[0]:
-            factor = 1.0
-        elif i < self.steps[1]:
-            factor = 0.1
-        else:
-            factor = 0.01
-        return factor
+        burn_in = self.cfg['SOLVER']['BURN_IN']
+        steps = eval(self.cfg['SOLVER']['STEPS'])
+
+        # Learning rate setup
+        def burnin_schedule(self,i):
+            if i < burn_in:
+                return pow(i / burn_in, 4)
+            elif i < steps[0]:
+                return 1.0
+            elif i < steps[1]:
+                return 0.1
+            else:
+                return 0.01
+        
+        self.scheduler = optim.lr_scheduler.LambdaLR(optimizer,burnin_schedule)
     
     def get_current_lr(self):
         return self.scheduler.get_last_lr()[0] *self.effective_batch_size
